47d73eb3a2995dd0b6eac7e02bf23198efbfeab5
vijayaad <vijayaad@MTLP-2075.in.mouritech.net>
2022-11-16 15:44:53 +0530

    demo commit

---
+++ practice/src/practice/Amstring_number.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class Amstring_number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+
+}
---
+++ practice/src/practice/Newpalindrome.java
@@ -0,0 +1,37 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Newpalindrome {
+
+	public static void main(String[] args) {
+
+		// to count no of digits in number
+		/*
+		 * int no=745282547; int count=0; while(no>0) { no= no/10; count++; }
+		 * System.out.println("Number of digit from a given number is  "+ count);
+		 */
+
+		Scanner scnr = new Scanner(System.in);
+		System.out.println("Enter a  new number");
+		int number = scnr.nextInt();
+		int originalnumb = number;
+		System.out.println("original number entered " + originalnumb);
+		System.out.println(number);
+		int rev = 0;
+		while (number != 0) {
+			rev = rev * 10 + number % 10; // 0+12345%10=5 50+4=54
+			System.out.println(rev);
+			number = number / 10;
+			System.out.println(number);
+		}
+		System.out.println("Reveresed number is " + rev);
+		if (originalnumb == rev) {
+			System.out.println("It is a palindrome number");
+		} else {
+			System.out.println("It is not a palindrome number");
+		}
+
+	}
+
+}
